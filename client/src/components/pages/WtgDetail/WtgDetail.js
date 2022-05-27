92653111be66ac1e84628480e3ded2b1646e52d3
yingmin99 <dudals98770@naver.com>
2022-05-27 16:20:01 +0900

    first

--- client/src/components/pages/WtgDetail/WtgDetail.js
+++ client/src/components/pages/WtgDetail/WtgDetail.js
@@ -3,7 +3,7 @@
 import React, { useEffect, useState } from 'react'
 
 
-import { useParams,useLocation } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
 
 import Auth from '../../../hoc/auth';
